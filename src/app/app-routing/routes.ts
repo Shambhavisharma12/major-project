19a4c6e6017ee282f556899a884dae9670e18b2e
shambhavisharma12 <shambhavisharma8jan@gmail.com>
2020-10-10 00:09:34 +0530

    for routing

--- src/app/app-routing/routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { SignupComponent } from '../signup/signup.component';
-import { AppComponent } from '../app.component';
-
-export const routes: Routes = [
-    { path: 'signup',  component: SignupComponent },
-    { path : 'home' , component :AppComponent},
-   // {  path: '', redirectTo: '/home', pathMatch: 'full'}
-  ];
