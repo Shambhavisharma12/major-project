1319dbec52112d3ad0a418e13c2b6be8e08967a2
shambhavisharma12 <shambhavisharma8jan@gmail.com>
2020-10-31 13:10:53 +0530

    register form

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -10,28 +10,28 @@ import { ReactiveFormsModule } from '@angular/forms'
 export class RegistrationComponent implements OnInit {
 
   
-  registerForm: FormGroup;
-  constructor( private formBuilder:FormBuilder) {
-    this.registerForm = formBuilder.group({
-      fullname:['',[Validators.required, Validators.minLength(2),Validators.maxLength(25)]],
+  // registerForm: FormGroup;
+  // constructor( private formBuilder:FormBuilder) {
+  //   this.registerForm = formBuilder.group({
+  //     fullname:['',[Validators.required, Validators.minLength(2),Validators.maxLength(25)]],
 
-      mobileno:['',[Validators.required, Validators.pattern]],
+  //     mobileno:['',[Validators.required, Validators.pattern]],
 
-      aleternatemobileno:['',[Validators.required, Validators.pattern]],
+  //     aleternatemobileno:['',[Validators.required, Validators.pattern]],
 
-      caddress:['',[Validators.required, Validators.minLength(10),Validators.maxLength(150)]],
+  //     caddress:['',[Validators.required, Validators.minLength(10),Validators.maxLength(150)]],
       
-      tov : ['',[Validators.required, ]]
+  //     tov : ['',[Validators.required, ]]
 
 
-    })     
-   }
+  //   })     
+  //  }
 
   ngOnInit(): void {
   }
-  postdata(){
-    console.log(this.registerForm);
-    console.log(this.registerForm.value)
-  }
+  // postdata(){
+  //   console.log(this.registerForm);
+  //   console.log(this.registerForm.value)
+  // }
 
 }
